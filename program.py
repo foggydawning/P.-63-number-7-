8e44e8278b47aa0408ee994030e86ad3f7e38d17
ILYA CHUB <iliachubich@gmail.com>
2019-05-21 19:41:27 +0300

    Создана программа

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 def add_random_list (list=[]):
     import random
-    for i in range (0, random.randrange(2,20,2)):
+    for i in range (0, random.randrange(4,20,2)):
         list.append(random.randint(0,100))
     print("Ваш список:", list, sep="\n")
     print("\n")
@@ -9,8 +9,19 @@ def add_random_list (list=[]):
 def razdel (list, list_=[]):
     for i in range (len(list)//2):
         list_.append(list.pop(0))
-    print(list_, list, sep="\n")
     return list_ , list
 
+def reverse (list_, list):
+    list.reverse()
+    for i in range (0,len(list_)-1):
+        list.insert(0, list_[i])
+    return list
+
+
 list=add_random_list()
-razdel(list)
+x=razdel(list)
+list_=x[0]
+list=x[1]
+list=reverse(list_, list)
+
+print("Отсортированный список:",list, sep="\n")
