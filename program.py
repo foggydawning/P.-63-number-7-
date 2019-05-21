a75b1c2479ab03766d459e0c89f288cda32b1977
ILYA CHUB <iliachubich@gmail.com>
2019-05-21 19:27:09 +0300

    Добавлена функция разделения списка на два

--- program.py
+++ program.py
@@ -1,9 +1,16 @@
 def add_random_list (list=[]):
     import random
-    for i in range (0, random.randrange(0,20,2)):
+    for i in range (0, random.randrange(2,20,2)):
         list.append(random.randint(0,100))
     print("Ваш список:", list, sep="\n")
     print("\n")
     return list
 
-add_random_list()
+def razdel (list, list_=[]):
+    for i in range (len(list)//2):
+        list_.append(list.pop(0))
+    print(list_, list, sep="\n")
+    return list_ , list
+
+list=add_random_list()
+razdel(list)
